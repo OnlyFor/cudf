15d82007294736f023e36daf2700f99f9637d114
JayjeetAtGithub <JayjeetAtGithub@users.noreply.github.com>
2024-09-09 13:47:14 -0700

    Rename col_id to column_id

--- cpp/benchmarks/ndsh/utilities.hpp
+++ cpp/benchmarks/ndsh/utilities.hpp
@@ -46,7 +46,7 @@ class table_with_names {
    *
    * @param col_name The name of the column
    */
-  [[nodiscard]] cudf::size_type col_id(std::string const& col_name) const;
+  [[nodiscard]] cudf::size_type column_id(std::string const& col_name) const;
   /**
    * @brief Append a column to the table
    *
